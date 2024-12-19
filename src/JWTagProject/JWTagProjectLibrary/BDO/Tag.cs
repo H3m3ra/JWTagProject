0c94a4d93e4070a546db438565e6bc2094d894ed
Hemera <j576569736572@gmail.com>
2024-12-19 21:28:16 +0100

    Fix Tag representation.

--- src/JWTagProject/JWTagProjectLibrary/BDO/Tag.cs
+++ src/JWTagProject/JWTagProjectLibrary/BDO/Tag.cs
@@ -10,6 +10,9 @@ public class Tag
     public Guid Id { get; set; }
 
     [Required]
+    public string Name { get; set; } = "";
+
+    [Required]
     public string Description { get; set; } = "";
 
     [Required]
